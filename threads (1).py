8fbb581a86140b4a62a56fec3f203eab13e71e9c
mohamedebrahim1085-debug <mohamedebrahim1085@gmail.com>
2025-12-10 20:16:46 +0200

    Add  threading
    Add  threading

---
+++ threads (1).py
@@ -0,0 +1,26 @@
+import threading  
+import time 
+
+def print_1():
+    print("Starting of a thread:", threading.current_thread().name)
+    time.sleep(2)  
+    print("Finishing of a thread:", threading.current_thread().name)
+
+def print_2():
+    print("Starting of a thread:", threading.current_thread().name)
+    print("Finishing of a thread:", threading.current_thread().name)
+
+a = threading.Thread(target=print_1, name="Thread-1")
+b = threading.Thread(target=print_2, name="Thread-2")
+
+a.start()
+b.start()
+
+
+a.join()  
+b.join()  
+
+print("Main thread: All threads have completed execution")
+
+
+
